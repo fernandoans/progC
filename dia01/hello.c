8fb9e64b28789580107e7b4a8ae6e7a7c541e0cb
Fernando Anselmo <fernando.anselmo74@gmail.com>
2022-02-20 14:55:10 -0300

    Aula 01

---
+++ dia01/hello.c
@@ -0,0 +1,15 @@
+/*
+Para compilar: gcc hello.c
+
+Para executar Windows: ./a.exe
+Para executar Linux: ./a.out
+
+*/
+#include <stdio.h>
+
+int main()
+{
+    // Nosso primeiro programa
+    printf("Hello World ! \n");
+    return 0;
+}
