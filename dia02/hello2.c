92a4d815f5faa439d6dbd5a9590b0aa1aefd9657
Fernando Anselmo <fernando.anselmo74@gmail.com>
2022-03-13 19:18:29 -0300

    Dia 02

---
+++ dia02/hello2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    // VARIÁVEIS
+    int x = 10; // 32 bits
+    printf("Hello World %d\n", x);
+    int y = x;
+    printf("O conteúdo de x é %d\n", x);
+    printf("O conteúdo de y é %d\n", y);
+    return 0;
+}
